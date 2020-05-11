1797995c9eb65afe2c030b33d155467811f057d1
McCoy Zhu <mz1945@nyu.edu>
2020-05-10 19:50:47 -0700

    Initial Commit

---
+++ GuessTheNumber WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  GuessTheNumber WatchKit Extension
+//
+//  Created by McCoy Zhu on 5/10/20.
+//  Copyright © 2020 McCoy Zhu. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ GuessTheNumber WatchKit Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  GuessTheNumber WatchKit Extension
+//
+//  Created by McCoy Zhu on 5/10/20.
+//  Copyright © 2020 McCoy Zhu. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
