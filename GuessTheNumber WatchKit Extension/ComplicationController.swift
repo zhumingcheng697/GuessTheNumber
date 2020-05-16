ba4105f0c243f572f261c977c6e80cbbf1302fd5
McCoy Zhu <mz1945@nyu.edu>
2020-05-15 22:02:10 -0700

    Added Complications

--- GuessTheNumber WatchKit Extension/ComplicationController.swift
+++ GuessTheNumber WatchKit Extension/ComplicationController.swift
@@ -50,7 +50,61 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     
     func getLocalizableSampleTemplate(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
         // This method will be called once per supported complication, and the results will be cached
-        handler(nil)
+        
+        switch complication.family {
+        case CLKComplicationFamily.circularSmall:
+            let tmpl = CLKComplicationTemplateCircularSmallSimpleImage()
+            tmpl.imageProvider = CLKImageProvider(onePieceImage: UIImage(named: "Complication/Circular")!)
+            handler(tmpl)
+            
+        case CLKComplicationFamily.extraLarge:
+            let tmpl = CLKComplicationTemplateExtraLargeSimpleImage()
+            tmpl.imageProvider = CLKImageProvider(onePieceImage: UIImage(named: "Complication/Extra Large")!)
+            handler(tmpl)
+            
+        case CLKComplicationFamily.graphicBezel:
+            let circularTmpl = CLKComplicationTemplateGraphicCircularImage()
+            circularTmpl.imageProvider = CLKFullColorImageProvider(fullColorImage: UIImage(named: "Complication/Graphic Bezel")!)
+            let tmpl = CLKComplicationTemplateGraphicBezelCircularText()
+            tmpl.circularTemplate = circularTmpl
+            tmpl.textProvider = CLKSimpleTextProvider(text: "Guess The Number", shortText: "Guess")
+            handler(tmpl)
+            
+        case CLKComplicationFamily.graphicCircular:
+            let tmpl = CLKComplicationTemplateGraphicCircularImage()
+            tmpl.imageProvider = CLKFullColorImageProvider(fullColorImage: UIImage(named: "Complication/Graphic Circular")!)
+            handler(tmpl)
+            
+        case CLKComplicationFamily.graphicCorner:
+            let tmpl = CLKComplicationTemplateGraphicCornerCircularImage()
+            tmpl.imageProvider = CLKFullColorImageProvider(fullColorImage: UIImage(named: "Graphic Corner")!)
+            handler(tmpl)
+            
+        case CLKComplicationFamily.modularSmall:
+            let tmpl = CLKComplicationTemplateModularSmallSimpleImage()
+            tmpl.imageProvider = CLKImageProvider(onePieceImage: UIImage(named: "Complication/Modular")!)
+            handler(tmpl)
+            
+        case CLKComplicationFamily.utilitarianSmall:
+            let tmpl = CLKComplicationTemplateUtilitarianSmallSquare()
+            tmpl.imageProvider = CLKImageProvider(onePieceImage: UIImage(named: "Complication/Utilitarian")!)
+            handler(tmpl)
+            
+        case CLKComplicationFamily.utilitarianSmallFlat:
+            let tmpl = CLKComplicationTemplateUtilitarianSmallFlat()
+            tmpl.imageProvider = CLKImageProvider(onePieceImage: UIImage(named: "Utilitatian Flat")!)
+            tmpl.textProvider = CLKSimpleTextProvider(text: "Guess The Number", shortText: "Guess")
+            handler(tmpl)
+            
+        case CLKComplicationFamily.utilitarianLarge:
+            let tmpl = CLKComplicationTemplateUtilitarianLargeFlat()
+            tmpl.imageProvider = CLKImageProvider(onePieceImage: UIImage(named: "Utilitatian Flat")!)
+            tmpl.textProvider = CLKSimpleTextProvider(text: "Guess The Number", shortText: "Guess")
+            handler(tmpl)
+            
+        default:
+            handler(nil)
+        }
     }
     
 }
